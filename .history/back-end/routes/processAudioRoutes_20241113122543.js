78edb1cf2911fe84ba1af982cde15649e906c6df
Isabelle Oktay <iso212@nyu.edu>
2024-11-20 19:34:22 +0100

    Remove .history folder from repository and modify file upload to include dynamic note range

--- .history/back-end/routes/processAudioRoutes_20241113122543.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const processAudioRouter = express.Router();
-import { processAudio } from "../controllers/audioFeaturesController.js";
-
-processAudioRouter.route("/process-audio").post(processAudio);
-
-export default processAudioRouter;
